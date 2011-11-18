d2bf298668b878daaf346dc57ff7b45731ac55ba
Luka Bozhich <lukas@gbg.bg>
2011-11-18 11:10:02 +0200

    cleared values

--- config/config.php
+++ config/config.php
@@ -4,15 +4,15 @@ $GLOBALS["DatabaseServer"]			= "localhost"; // Added - Necrojelly 2009-12-10
 //Your MySQL Database Name
 $GLOBALS["DatabaseName"]			= "webdkp";
 //The Username linked to your MySQL DB
-$GLOBALS["DatabaseUsername"]		= "root";
+$GLOBALS["DatabaseUsername"]		= "";
 //The password for the MySQL user above
-$GLOBALS["DatabasePassword"]		= "jipxnmzn";
+$GLOBALS["DatabasePassword"]		= "";
 //Title of the site Default:WebDKP
 $GLOBALS["SiteTitle"]			 	= "";
 $GLOBALS["SiteKeywords"]     	 	= "";
-$GLOBALS["SiteDescription"]		 	= "Path Of Salvation DKP System";
+$GLOBALS["SiteDescription"]		 	= "";
 //The main root of your website - Ex: http://www.mysite.com/
 // This DOES NOT include the WebDKP directory
-$GLOBALS["SiteRoot"]			= "http://dkp.lbds.co.cc/";
+$GLOBALS["SiteRoot"]			= "";
 $GLOBALS["SupportedExtensions"] 	= array("php","phps","aspx","html","htm","js","css");
 ?>
